544753fe65516c775d92aaf45c12ecd46d728b9d
M.Faqih Ridho <74019017+MFaqihRidho@users.noreply.github.com>
2021-08-12 15:02:04 +0700

    Add files via upload

---
+++ hut ri 76 website/js/script.js
@@ -0,0 +1,25 @@
+// const dropdown = document.querySelector('.dropdown-content')
+const droptrigger = document.querySelector('.dropdown-trigger')
+M.Dropdown.init(droptrigger,{
+    coverTrigger:false,
+    constrainWidth:false
+});
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider,{
+    indicators:false,
+    height:400,
+    interval:3000
+});
+
+const collapsible = document.querySelector('.collapsible')
+M.Collapsible.init(collapsible);
+
+const select = document.querySelectorAll('select');
+M.FormSelect.init(select);
+
+
+const scroll = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scroll,{
+    scrollOffset: 50
+});
